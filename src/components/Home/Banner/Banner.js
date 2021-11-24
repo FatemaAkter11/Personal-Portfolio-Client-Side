43ffb18959dd8ecd7338e33ab5316df53aa0b724
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-11-25 00:10:30 +0600

    banner part completed

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -2,12 +2,21 @@ import React from 'react';
 import './Banner.css';
 import Typed from 'react-typed';
 import { Link } from 'react-router-dom';
+import bubble from '../../../images/bubble.png';
 
 const Banner = () => {
     return (
         <div className="banner">
             <div className="banner-main">
-                <h1 className="fw-bolder title ">Hello, My Name is <span className="fw-bolder fs-1 text">Fatema Akter</span> <br /> And I am a ...</h1>
+                <div className="bubbles">
+                    <img src={bubble} alt="" />
+                    <img src={bubble} alt="" />
+                    <img src={bubble} alt="" />
+                    <img src={bubble} alt="" />
+                    <img src={bubble} alt="" />
+                    <img src={bubble} alt="" />
+                </div>
+                <h1 className="fw-bolder title ">Hello, This is <span className="fw-bolder text">Fatema Akter</span> <br /> And I am a ...</h1>
                 <Typed
                     className="typed-text fw-bolder"
                     strings={[' Web Designer', 'Frontend Developer', 'Junior Web Developer']}
@@ -16,7 +25,9 @@ const Banner = () => {
                     loop
                 />
                 <Link to='/contacts' className="btn fw-bold mt-2">Contact Me</Link>
+
             </div>
+
 
         </div>
     );
