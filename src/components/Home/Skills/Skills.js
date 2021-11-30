79a0fd5af67e918b158ea5f230c2f2f1a3a07690
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-11-30 12:38:38 +0600

    my skills update

--- src/components/Home/Skills/Skills.js
+++ src/components/Home/Skills/Skills.js
@@ -16,7 +16,7 @@ const Skills = () => {
                                 <h6 className="font-weight-bold">HTML</h6>
                                 <h6 className="font-weight-bold">95%</h6>
                             </div>
-                            <div className="progress">
+                            <div className="progress animate__animated animate__rotateInDownLeft">
                                 <div style={{ width: '95%' }} className="progress-bar bg-success" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
                         </div>
@@ -25,7 +25,7 @@ const Skills = () => {
                                 <h6 className="font-weight-bold">CSS</h6>
                                 <h6 className="font-weight-bold">85%</h6>
                             </div>
-                            <div className="progress">
+                            <div className="progress animate__animated animate__rotateInDownLeft">
                                 <div style={{ width: '85%' }} className="progress-bar bg-info" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
                         </div>
@@ -34,7 +34,7 @@ const Skills = () => {
                                 <h6 className="font-weight-bold">JAVASCRIPT</h6>
                                 <h6 className="font-weight-bold">80%</h6>
                             </div>
-                            <div className="progress">
+                            <div className="progress animate__animated animate__rotateInDownLeft">
                                 <div style={{ width: '80%' }} className="progress-bar bg-danger" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
                         </div>
@@ -43,7 +43,7 @@ const Skills = () => {
                                 <h6 className="font-weight-bold">BOOTSTRAP</h6>
                                 <h6 className="font-weight-bold">90%</h6>
                             </div>
-                            <div className="progress">
+                            <div className="progress animate__animated animate__rotateInDownLeft">
                                 <div style={{ width: '90%' }} className="progress-bar bg-dark" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
                         </div>
@@ -54,7 +54,7 @@ const Skills = () => {
                                 <h6 className="font-weight-bold">TAILWIND</h6>
                                 <h6 className="font-weight-bold">85%</h6>
                             </div>
-                            <div className="progress">
+                            <div className="progress animate__animated animate__rotateInDownLeft">
                                 <div style={{ width: '85%' }} className="progress-bar bg-warning" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
                         </div>
@@ -63,7 +63,7 @@ const Skills = () => {
                                 <h6 className="font-weight-bold">MATERIAL UI</h6>
                                 <h6 className="font-weight-bold">75%</h6>
                             </div>
-                            <div className="progress">
+                            <div className="progress animate__animated animate__rotateInDownLeft">
                                 <div style={{ width: '75%' }} className="progress-bar bg-danger" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
                         </div>
@@ -72,7 +72,7 @@ const Skills = () => {
                                 <h6 className="font-weight-bold">NODE JS</h6>
                                 <h6 className="font-weight-bold">95%</h6>
                             </div>
-                            <div className="progress">
+                            <div className="progress animate__animated animate__rotateInDownLeft">
                                 <div style={{ width: '95%' }} className="progress-bar bg-success" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
                         </div>
@@ -81,7 +81,7 @@ const Skills = () => {
                                 <h6 className="font-weight-bold">MONGODB</h6>
                                 <h6 className="font-weight-bold">98%</h6>
                             </div>
-                            <div className="progress">
+                            <div className="progress animate__animated animate__rotateInDownLeft">
                                 <div style={{ width: '98%' }} className="progress-bar bg-info" role="progressbar" aria-valuenow="98" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
                         </div>
