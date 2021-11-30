f087b1a8d2cb1ee54b8fb0ba86f2db35cf33af59
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-11-30 19:14:27 +0600

    updated

--- src/components/Home/Blogs/Blogs.js
+++ src/components/Home/Blogs/Blogs.js
@@ -3,6 +3,10 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
+            <div className="position-relative d-flex align-items-center justify-content-center">
+                <h1 className="display-1 text-uppercase text-white textP">Blog</h1>
+                <h1 className="position-absolute text-uppercase text-primary">Blogs</h1>
+            </div>
             <h2 className="text-center fw-bold my-5 animate__animated animate__lightSpeedInRight">Blogs Coming Soon</h2>
         </div>
     );
